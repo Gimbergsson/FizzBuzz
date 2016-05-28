27aebf5b35cd9f4d32b23a6eaa89e631b1870187
Dennis Gimbergsson <dennisgimbergsson@live.se>
2016-05-28 22:56:56 +0200

    added textview to view the console output

--- app/src/main/java/com/free/dennisg/fizzbuzz/MainActivity.java
+++ app/src/main/java/com/free/dennisg/fizzbuzz/MainActivity.java
@@ -2,6 +2,9 @@ package com.free.dennisg.fizzbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
 
         TextView consoleOutTextView = (TextView) findViewById(R.id.textView);
+        final TextView fizzBuzzOutput = (TextView) findViewById(R.id.textView2);
+        final EditText fizzBuzzEditText = (EditText) findViewById(R.id.editText);
+        final Button fizzBuzzButton = (Button) findViewById(R.id.button);
+
+        assert fizzBuzzButton != null;
+        fizzBuzzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double fizzBuzzNumber;
+                if (fizzBuzzEditText.getText().length() > 0) {
+                    fizzBuzzNumber = Double.valueOf(fizzBuzzEditText.getText().toString());
+                }else{
+                    fizzBuzzNumber = 0;
+                }
+
+                String fizzBuzzText = "";
+
+                if (fizzBuzzNumber % 3 == 0){
+                    fizzBuzzText += "Fizz";
+                }
+                if (fizzBuzzNumber % 5 == 0){
+                    fizzBuzzText += (fizzBuzzText != "" ? "" : "") + "Buzz";
+                }
+
+                if(fizzBuzzNumber % 5 != 0 && fizzBuzzNumber % 3 != 0){
+                    //fizzBuzzText += String.valueOf(fizzBuzzNumber);
+                    fizzBuzzText += "Neither";
+                }
+
+                assert fizzBuzzOutput != null;
+                fizzBuzzOutput.setText(fizzBuzzText);
+
+            }
+        });
+
 
         String text = "";
 
